8547023a555375ea8843bbb599ab731fc12cbf7f
ROHITSAIR <rohitravipat900@gmail.com>
2025-11-15 12:16:09 +0530

    worke on some basic examples on routes

--- src/app.js
+++ src/app.js
@@ -4,13 +4,45 @@ const express=require("express");
 
 const app=express();
 
+
+//this will match all the http methods(get,post,put,delete,patch etc)
 app.use("/test",(req,res)=>{
-    res.send("Hello from the server");
+    res.send("test");
 });
 
-app.use("/hello",(req,res)=>{
-    res.send("Hello from the hello");
+//post
+app.post("/user",(req,res)=>{
+res.send("Post mtd succesfully fetched");
+
+
+//put
+app.put("/user",(req,res)=>{
+    res.send("Put mtd is used here");
 })
+//delete
+app.delete("/user",(req,res)=>{
+    res.send("Delete mtd is used here");
+})
+})
+//this will match only get /user
+app.get("/user",(req,res)=>{
+    res.send({firstName : "Rohit",laastName : "R",age:22}); 
+})
+app.use("/user",(req,res)=>{
+    res.send("used app.use mtd not app.patch");
+})
+
+
+// app.use("/hello/run",(req,res)=>{
+//     res.send("Hello,run!!!!!");
+// })
+// app.use("/hello",(req,res)=>{
+//     res.send("Hello,hello,hello");
+// });
+// app.use("/",(req,res)=>
+// {
+//     res.send("Hello from the first point")
+// })
 
 
 app.listen(3000,()=>{
