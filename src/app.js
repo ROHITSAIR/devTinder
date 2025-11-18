6cb721d1ded82c390cbba891554251816a0ef10c
ROHITSAIR <rohitravipat900@gmail.com>
2025-11-18 15:27:31 +0530

    practiced on routes and middlewares

--- src/app.js
+++ src/app.js
@@ -1,50 +1,21 @@
-// console.log("Starting a new file");
-
 const express=require("express");
-
 const app=express();
+const {adminAuth,userAuth}=require('./middleware/auth')
 
-
-//this will match all the http methods(get,post,put,delete,patch etc)
-app.use("/test",(req,res)=>{
-    res.send("test");
-});
-
-//post
-app.post("/user",(req,res)=>{
-res.send("Post mtd succesfully fetched");
-
-
-//put
-app.put("/user",(req,res)=>{
-    res.send("Put mtd is used here");
+app.use("/admin",adminAuth);
+app.get("/admin/getData",(req,res)=>{
+  res.send("Admin data is added");
 })
-//delete
-app.delete("/user",(req,res)=>{
-    res.send("Delete mtd is used here");
+app.get("/admin/deleteData",(req,res)=>{
+  res.send("Admin data is deleted");
 })
+app.get("/admin/login",(req,res)=>{
+  res.send("Admin login");
 })
-//this will match only get /user
-app.get("/user",(req,res)=>{
-    res.send({firstName : "Rohit",laastName : "R",age:22}); 
+app.use("/user",userAuth,(req,res)=>{
+  res.send("Data  is deleted");
+
 })
-app.use("/user",(req,res)=>{
-    res.send("used app.use mtd not app.patch");
-})
-
-
-// app.use("/hello/run",(req,res)=>{
-//     res.send("Hello,run!!!!!");
-// })
-// app.use("/hello",(req,res)=>{
-//     res.send("Hello,hello,hello");
-// });
-// app.use("/",(req,res)=>
-// {
-//     res.send("Hello from the first point")
-// })
-
-
 app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
-});
+  console.log("Server is running on port 3000");
+})
---
+++ src/middleware/auth.js
@@ -0,0 +1,33 @@
+const adminAuth=(req,res,next)=>
+{
+    console.log("Admin is checking");
+    const token="xyz";
+    const isAdmin=token==="xyz";
+    if(!isAdmin)
+    {
+     res.status(401).send("Unauthorized access");
+
+    }
+    else{
+                     next();
+
+
+    }
+
+}
+const userAuth=(req,res,next)=>
+{
+    console.log("User is checking");
+    const token="xyz";
+    const isAdmin=token==="xyz";
+    if(!isAdmin)
+    {
+     res.status(401).send("Unauthorized access");
+
+    }
+    else{
+    
+    next();
+    }}
+
+module.exports={adminAuth,userAuth};
